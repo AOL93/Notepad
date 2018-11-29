18ff2c44121dfa4cccc9936c0dccac3dfbee0060
Adam Oleksiak <adam93@post.pl>
2018-11-29 23:50:45 +0100

    Partial print fix

--- src/sample/FilePrinter.java
+++ src/sample/FilePrinter.java
@@ -1,11 +1,8 @@
 package sample;
 
 import javafx.print.*;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
-import javafx.scene.transform.Scale;
 
 public class FilePrinter {
 
@@ -14,40 +11,33 @@ public class FilePrinter {
     private PageLayout layout;
     private PrinterAttributes attr;
 
-    private double scaleX,scaleY;
-
 
     public FilePrinter() {
         this.printer = Printer.getDefaultPrinter();
         this.layout = printer.createPageLayout(
                 Paper.A4,
                 PageOrientation.PORTRAIT,
-                Printer.MarginType.HARDWARE_MINIMUM);
+                Printer.MarginType.DEFAULT);
         this.attr = printer.getPrinterAttributes();
         this.job = PrinterJob.createPrinterJob();
     }
 
     public void print(String text) {
         TextFlow toPrint = new TextFlow(new Text(text));
+        //FIXME PageRange atm 9999
+        //FIXME Printing only first page
 
-        scaleX = 1 - layout.getPrintableWidth() / toPrint.getBoundsInParent().getWidth();
-        scaleY = 1 - layout.getPrintableHeight() / toPrint.getBoundsInParent().getHeight();
-        //FIXME PageRange atm 9999 toPrint.getBoundsInParent().getHeight() / layout.getPrintableHeight()
-        //FIXME Margins ????
+        double numberOfPages = Math.ceil(layout.getPrintableHeight() / toPrint.getBoundsInParent().getHeight());
 
-        System.out.println(scaleX + " = " + layout.getPrintableWidth() + "/" + toPrint.getBoundsInParent().getWidth());
-        System.out.println(scaleY + " = " + layout.getPrintableHeight() + "/" + toPrint.getBoundsInParent().getHeight());
-
-        Scale scale = new Scale(scaleX,scaleY);
-
-        toPrint.getTransforms().add(scale);
         toPrint.setMaxWidth(layout.getPrintableWidth());
+        toPrint.setMaxHeight(layout.getPrintableHeight());
+
+        System.out.println(numberOfPages);
 
         if(job != null && job.showPrintDialog(null)) {
             if(job.printPage(layout,toPrint)) {
                 job.endJob();
             }
         }
-        toPrint.getTransforms().remove(scale);
     }
 }
